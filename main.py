9a141a8117a2d4a8356f6428e0cdb702afca18c6
devcalibran <devcalibran@gmail.com>
2025-08-10 15:08:33 +0200

    update main.py to print word count of local book

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text():
+    with open("./books/frankenstein.txt") as f:
+        return f.read()
+
+def get_word_count(text: str):
+    return len(text.split())
+
+def main():
+    text = get_book_text()
+    count = get_word_count(text)
+    print(f"{count} words found in the document")
+
+main()
