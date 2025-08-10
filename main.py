86a284eff04255c1a4733441697fd27b358a3cce
devcalibran <devcalibran@gmail.com>
2025-08-10 17:30:08 +0200

    added sys import to take path argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_word_count, get_character_count
 from stats import get_sorted_character_counts
+import sys
 
 
 def get_book_text(path: str):
@@ -8,8 +9,12 @@ def get_book_text(path: str):
 
 
 def main():
+    args = sys.argv
+    if len(args) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    path = "./books/frankenstein.txt"
+    path = args[1]
     print(f"Analyzing book found at {path}...")
     text = get_book_text(path)
     count = get_word_count(text)
